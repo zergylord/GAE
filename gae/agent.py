everything working now -- fixed silly gradient sign error

gae/agent.py
@@ -2,6 +2,7 @@ import os
 import tensorflow as tf
 import numpy as np
 import time
+from scipy.stats import truncnorm
 from .ops import *
 
 class GAE_Agent(object):
@@ -10,14 +11,18 @@ class GAE_Agent(object):
         self.writer = writer
         self.x_dim = env.observation_space.shape[0]
         self.u_dim = 1
-        self.hid_dim = 256
+        self.hid_dim1 = 128
+        self.hid_dim2 = 300
+        self.sigma = .11
         self.sess = sess
         self.env = env
-        self.gamma = .99
+        self.gamma = .995
         self.build_model()
+        self.ret = 0.0
+        tf.Summary(value=[tf.Summary.Value(tag="avg R", simple_value=self.ret)])
         self.merged = tf.merge_all_summaries()
-        self.episodes_per_update = 20
-        self.max_steps_per_episode = 1000
+        self.episodes_per_update = 120
+        self.max_steps_per_episode = 100
         self.reset_hist()
     def reset_hist(self):
         self.x_hist = []
@@ -39,6 +44,7 @@ class GAE_Agent(object):
             rewards.append(reward)
             if term or step == self.max_steps_per_episode:
                 x = self.env.reset() #only on term?
+                self.ret = np.sum(rewards)
                 self.R_hist = self.R_hist + compute_return(rewards,self.gamma)
                 step = 0
                 rewards = []
@@ -63,8 +69,9 @@ class GAE_Agent(object):
         x = np.expand_dims(x,0)
         cur_mu = self.sess.run(self.mu,feed_dict={self.x_:x})
         cur_mu = np.squeeze(cur_mu,0)
-        cur_sigma = 1.0 #network could learn this as well as mu
+        cur_sigma = self.sigma #network could learn this as well as mu
         return cur_mu+np.random.randn(self.u_dim)*cur_sigma
+        #return truncnorm.rvs(cur_mu,cur_sigma,self.u_dim)
     def build_model(self):
         #inputs
         self.x_ = tf.placeholder('float32',[None,self.x_dim])
@@ -74,18 +81,22 @@ class GAE_Agent(object):
         tf.scalar_summary('avg Returns',tf.reduce_mean(self.R_))
         tf.histogram_summary('actions',self.u_)
         #network definition
-        self.hid1 = linear(self.x_,self.hid_dim,'hid1',tf.nn.relu)
-        self.last_hid = linear(self.hid1,self.hid_dim,'hid2')
-        self.mu = linear(self.last_hid,self.u_dim,'mu')
-        self.baseline = linear(self.last_hid,1,'baseline')
-        self.log_p = tf.log(tf.clip_by_value(norm_pdf(self.u_,self.mu),eps,float('inf')))
-        tf.histogram_summary('log p',self.log_p)
+        self.hid1 = linear(self.x_,self.hid_dim1,'hid1',tf.nn.relu)
+        #self.hid2= linear(self.hid1,self.hid_dim2,'hid2',tf.nn.relu)
+        last_hid = self.hid1
+        self.mu = linear(last_hid,self.u_dim,'mu',bias=False)
+        #self.mu = tf.tanh(self.mu)*2.0 #normalize to action range
+        tf.histogram_summary('mu',self.mu)
+        self.baseline = linear(last_hid,1,'baseline')
+        self.log_p = tf.log(tf.clip_by_value(norm_pdf(self.u_,self.mu,self.sigma),eps,float('inf')))
+        #self.log_p = tf.log(norm_pdf(self.u_,self.mu,self.sigma))
 
         self.b_loss = tf.reduce_mean(tf.square(self.R_-self.baseline))
-        tf.scalar_summary('baseline error',self.b_loss)
-        self.u_loss = tf.reduce_mean(self.log_p*(self.R_-tf.stop_gradient(self.baseline)))
+        tf.scalar_summary('baseline',tf.reduce_mean(self.baseline))
+        tf.scalar_summary('baseline - R',tf.reduce_mean(self.R_-self.baseline))
+        self.u_loss = tf.reduce_mean(-self.log_p*(self.R_-tf.stop_gradient(self.baseline)))
         tf.scalar_summary('action prob * A',self.u_loss)
-        self.net_loss = self.b_loss+self.u_loss
+        self.net_loss = 1.0*self.b_loss+self.u_loss
         tf.scalar_summary('loss',self.net_loss)
         #optimizer
         self.optim = tf.train.AdamOptimizer(1e-4)

gae/ops.py
@@ -1,17 +1,20 @@
 import tensorflow as tf
 import numpy as np
-def linear(in_,out_dim,name,activation_fn=None):
+def linear(in_,out_dim,name,activation_fn=None,bias=True):
     in_dim = in_.get_shape()[1]
     with tf.variable_scope(name):
         W = tf.get_variable('W',[in_dim,out_dim],tf.float32)
-        b = tf.get_variable('b',[out_dim],tf.float32)
-        out = tf.matmul(in_,W)+b
+        out = tf.matmul(in_,W)
+        if bias:
+            b = tf.get_variable('b',[out_dim],tf.float32)
+            out = out + b
         if activation_fn != None:
             return activation_fn(out)
         else:
             return out
 eps = 1e-8
 def norm_pdf(x,mu=0.0,sigma=1.0):
+    #var = tf.square(sigma)
     var = tf.clip_by_value(tf.square(sigma),eps,float('inf'))
     return tf.rsqrt(2.0*np.pi*var)*tf.exp(-tf.square(x-mu)/(2.0*var))
     